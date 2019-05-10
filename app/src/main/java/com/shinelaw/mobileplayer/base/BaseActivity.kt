96e8b1a937362f10a2e1beda74accb1aa4c2f1d3
shinelaw <240272750@qq.com>
2019-05-10 17:17:27 +0800

    new files

--- app/src/main/java/com/shinelaw/mobileplayer/base/BaseActivity.kt
+++ app/src/main/java/com/shinelaw/mobileplayer/base/BaseActivity.kt
@@ -1,12 +1,13 @@
-package com.shinelaw.mobileplayer
+package com.shinelaw.mobileplayer.base
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.shinelaw.mobileplayer.R
 
-class MainActivity : AppCompatActivity() {
+class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_base)
     }
 }
---
+++ app/src/main/java/com/shinelaw/mobileplayer/ui/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.shinelaw.mobileplayer.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.shinelaw.mobileplayer.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
